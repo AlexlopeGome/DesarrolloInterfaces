bd65cc15a416940a93c38b9e266b6537402ef91b
AlexlopeGome <92096480+AlexlopeGome@users.noreply.github.com>
2022-11-02 19:04:25 +0100

    ejercicio 3 linea141

--- prIntroduccionFX/src/application/Main.java
+++ prIntroduccionFX/src/application/Main.java
@@ -115,31 +115,62 @@ public class Main extends Application {
 //			primaryStage.getIcons().add(new Image("/application/icono.png"));
 //			primaryStage.show();
 		
-		// Ejerccicio 1 
+//		// Ejerccicio  2
+//			StackPane panel=new  StackPane();
+//			Rectangle rect = new Rectangle(20,20,300,300);
+//		    rect.setFill(Color.DARKGREEN);
+//		    rect.setLayoutX(0);
+//		    rect.setLayoutY(0);
+//		    Rectangle rect2 = new Rectangle(20,20,250,250);
+//		    rect2.setFill(Color.GREEN);
+//		    rect2.setLayoutX(1);
+//		    rect2.setLayoutY(1);
+//		    Rectangle rect3 = new Rectangle(20,20,200,200);
+//		    rect3.setFill(Color.AQUAMARINE);
+//		    rect3.setLayoutX(2);
+//		    rect3.setLayoutY(2);
+//		    
+//		    panel.getChildren().addAll(rect,rect2,rect3);
+//			Scene scene= new Scene(panel,300,300);
+//			primaryStage.setScene(scene);
+//			primaryStage.setTitle("Introducion JavaFX");
+//			primaryStage.getIcons().add(new Image("/application/icono.png"));
+//			primaryStage.show();
 		
-			StackPane panel=new  StackPane();
-			Rectangle rect = new Rectangle(20,20,300,300);
-		    rect.setFill(Color.DARKGREEN);
-		    rect.setLayoutX(0);
-		    rect.setLayoutY(0);
-		    Rectangle rect2 = new Rectangle(20,20,250,250);
-		    rect2.setFill(Color.GREEN);
-		    rect2.setLayoutX(1);
-		    rect2.setLayoutY(1);
-		    Rectangle rect3 = new Rectangle(20,20,200,200);
-		    rect3.setFill(Color.AQUAMARINE);
-		    rect3.setLayoutX(2);
-		    rect3.setLayoutY(2);
-		    
-		    panel.getChildren().addAll(rect,rect2,rect3);
-			Scene scene= new Scene(panel,300,300);
+			
+	// ejecicio 3
+			
+			Button btn1= new Button("1");
+			Button btn2= new Button("2");
+			Button btn3= new Button("3");
+			Button btn4= new Button("4");
+			Button btn5= new Button("5");
+			Button btn6= new Button("6");
+			Button btn7= new Button("7");
+			Button btn8= new Button("8");
+			Button btn9= new Button("9");
+			Button btn0= new Button("0");
+			Button btnLlamar= new Button("Llamar");
+			Button btnColgar= new Button("Colgar");
+			GridPane panel =new GridPane();
+			panel.add(btn1, 0, 0);
+			panel.add(btn2, 1, 0);
+			panel.add(btn3, 2, 0);
+			panel.add(btn4, 0, 1);
+			panel.add(btn5, 1, 1);
+			panel.add(btn6, 2, 1);
+			panel.add(btn7, 0, 2);
+			panel.add(btn8, 1, 2);
+			panel.add(btn9, 2, 2);
+			panel.add(btn0, 1, 3);
+			panel.add(btnLlamar, 3, 1);
+			panel.add(btnColgar, 3, 2);
+			Scene scene= new Scene(panel,400,300);
+			
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Introducion JavaFX");
 			primaryStage.getIcons().add(new Image("/application/icono.png"));
-			primaryStage.show();
-		
-			
-			
+			primaryStage.show();	
 			
 		} catch(Exception e) {
 			e.printStackTrace();
